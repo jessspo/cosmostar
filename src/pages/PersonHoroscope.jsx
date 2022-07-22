19c93645f1075aa030383c64465bad0f84ae5d50
jessspo <67518904+jessspo@users.noreply.github.com>
2022-07-22 16:59:06 +0200

    fake data added and responsiveness

--- src/pages/PersonHoroscope.jsx
+++ src/pages/PersonHoroscope.jsx
@@ -5,24 +5,23 @@ import "./PersonHoroscope.css";
 const PersonHoroscope = (props) => {
   return (
     <div 
-    style={{backgroundImage: `url(${horoscope})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    width:'100%' ,
-    height: '100%',
-    marginTop: 0,
-    }}>
+      style={{backgroundImage: `url(${horoscope})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+
+      width:'100%' ,
+      height: '100%',
+      marginTop: 0,
+      }}>
       
-     <div className='shadow_horoscope'>
-            <div>
+      <div className='shadow_horoscope'>
             <h1>Gemini</h1>
-            </div>
-            <div className='box'>
+      
+          <div className='box'>
             <p>The twins are used to describe the sign of Gemini because typical Gemini people love to share ideas with someone else. The most important thing for a Gemini is communication. Gemini people are almost always curious about lots of different things. A typical Gemini person usually enjoys being with other people, and loves to talk about the latest thing he is doing. People usually like their Gemini friends, because they have something interesting to share with everybody.</p>
 
-            <p>Many Gemini people are talented at writing, teaching and public speaking. Some are gifted dancers. Many of them work in the fields of radio, television and newspapers because they love to be around when news is being created. It is especially important for Gemini people to travel and meet new people.</p>
-
- </div>
+            <p>Many Gemini people are talented at writing, teaching and public speaking. It is especially important for Gemini people to travel and meet new people.</p>
+          </div>
       </div>
     </div>
   )
